aade5ffda5c5b920216fa5f8e9ecb075d8e3ab82
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2026-09-05 22:56:11 +0200

    feat: c sully implemented

--- C/Sully.c
+++ C/Sully.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 int main() {
-char str[]="#include <stdio.h>%1$c#include <unistd.h>%1$cint main() {%1$cchar str[]=%2$c%3$s%2$c;%1$cint n=%4$d;%1$cchar b[100];%1$csprintf(b,%2$cSully_%5$cd.c%2$c,n);%1$cFILE *fd=fopen(b,%2$cw%2$c);%1$cfprintf(fd,str,10,34,str,n-1,37);%1$cfclose(fd);%1$cif (n<0) return 0;%1$cchar *a[]={%2$c-c%2$c,%2$c'echo $0 -o $0 && ./$0'%2$c,b,0};%1$cexecve(%2$cbash%2$c,a,NULL);%1$c}%1$c";
+char str[]="#include <stdio.h>%1$c#include <unistd.h>%1$c#include <string.h>%1$c#include <sys/wait.h>%1$cint main() {%1$cchar str[]=%2$c%3$s%2$c;%1$cint n=%4$d;%1$cn--;%1$cchar b[100];%1$csprintf(b,%2$cSully_%5$cd.c%2$c,n);%1$cFILE *fd=fopen(b,%2$cw%2$c);%1$cfprintf(fd,str,10,34,str,n,37);%1$cfclose(fd);%1$cchar c[100];%1$cmemcpy(c+2,b,strlen(b));%1$cc[0]='.';c[1]='/';c[strlen(b)]=0;%1$cif(!fork()) execlp(%2$cgcc%2$c,%2$cgcc%2$c,b,%2$c-o%2$c,c,NULL);%1$celse {wait(NULL);if(n>=0)execlp(c,c,NULL);}%1$c}%1$c";
 int n=5;
+n--;
 char b[100];
 sprintf(b,"Sully_%d.c",n);
 FILE *fd=fopen(b,"w");
-fprintf(fd,str,10,34,str,n-1,37);
+fprintf(fd,str,10,34,str,n,37);
 fclose(fd);
-if (n<0) return 0;
-char *a[]={"-c","'echo $0 -o $0 && ./$0'",b,0};
-execve("bash",a,NULL);
+char c[100];
+memcpy(c+2,b,strlen(b));
+c[0]='.';c[1]='/';c[strlen(b)]=0;
+if(!fork()) execlp("gcc","gcc",b,"-o",c,NULL);
+else {wait(NULL); if(n>=0)execlp(c,c,NULL);}
 }
