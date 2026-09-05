cb276fd4338599909fbbb0e7abdfb9a15a60fda4
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2026-09-05 17:49:04 +0200

    feat: C Grace

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-// Outside comment here
+// Outside comment
 
 void foobar() {
-    // Inside comment
-    char str[] = "#include <stdio.h>%1$c%1$c// Outside comment here%1$c%1$cvoid foobar() {%1$c    // Inside comment%1$c    char str[] = %2$c%3$s%2$c;%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main() {%1$c    foobar();%1$c}%1$c";
+    char str[] = "#include <stdio.h>%1$c%1$c// Outside comment%1$c%1$cvoid foobar() {%1$c    char str[] = %2$c%3$s%2$c;%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main() {%1$c    // Inside comment%1$c    foobar();%1$c}%1$c";
     printf(str, 10, 34, str);
 }
 
 int main() {
+    // Inside comment
     foobar();
 }
